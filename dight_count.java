89c6ca0ce674da911f8afb600c0dba598ff4ba12
hrishabh singh <hrishabh1980@gmail.com>
2026-05-09 09:40:57 +0530

    Create dight_count.java

---
+++ dight_count.java
@@ -0,0 +1,16 @@
+ublic class Main
+{
+	public static void main(String[] args) {
+		int count =0;
+		int n=arr.length;
+		while(n<0){
+		    count++;
+		    n=n/10
+		}
+		System.out.println(count);
+	
+		
+		
+		
+	}
+}
