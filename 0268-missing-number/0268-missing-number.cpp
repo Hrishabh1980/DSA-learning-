6b55db235cf0bc86090e996e93fc07b1bba85668
hrishabh singh <hrishabh1980@gmail.com>
2026-08-13 11:17:04 +0530

    Time: 2 ms (22.36%), Space: 21.7 MB (94.37%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-        //case 1
-        if(nums[0] != 0)return 0;
-        //case 2 
-        if(nums[n-1] != n)return n;
-        for(int i =1;i<nums.size();i++){
-            if(nums[i] != i){
-            //case 3
-            return i;
-            }
+        int ans=nums.size();
+        for(int i=0;i<nums.size();i++)
+        {
+            ans^=i^nums[i];
+
         }
-        return 0;
+        return ans;
+        
     }
 };
